f7cbaf186af65089d18afc8f0f3c18f5d754659f
AhmedAbdelaty2 <ahmedabdelaty2021@gmail.com>
2022-03-04 15:31:53 +0200

    lab4 is updated

--- lab4/config.php
+++ lab4/config.php
@@ -3,5 +3,5 @@ define("_driver_", "mysql");
 define("_host_", "localhost");
 define("_database_", "northwind");
 define("_username_", "root");
-define("_password_", "Leavetheduckalone22");
+define("_password_", "");
 define("_Pager_size_",5);
