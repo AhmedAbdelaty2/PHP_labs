14822553bb2c1ba47a844d03b5fd49119ed47626
AhmedAbdelaty2 <ahmedabdelaty2021@gmail.com>
2022-03-07 18:44:53 +0200

    lab5

--- lab4/index.php
+++ lab4/index.php
@@ -14,4 +14,11 @@ $next_link = "http://localhost/lab4/index.php?index=$next_index";
 $previous_index = (($index - _Pager_size_)>=0)?$index - _Pager_size_:0;
 $previous_link = "http://localhost/lab4/index.php?index=$previous_index";
 
-require_once("views/table.php");
+if(isset($_GET["glass"]) && is_numeric($_GET["glass"]) && $_GET["glass"] >= 0){
+    require_once("views/details.php");
+}elseif(isset($_GET["search"])){
+    $all_requrds = $all_requrds->where("product_name",$_GET["search"]);
+    require_once("views/table.php");
+}else{
+    require_once("views/table.php");
+}
--- lab4/views/table.php
+++ lab4/views/table.php
@@ -7,13 +7,21 @@ foreach ($all_requrds as $item) {
         echo "<tr>";
         echo "<td> ID </td>";
         echo "<td> Name </td>";
+        echo "<td> image </td>";
+        echo "<td> details </td>";
         
         echo "</tr>";
     }
     echo "<tr>";
-   
+    
+        $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        $actual_link = explode("?", $actual_link)[0];
+        $more = $actual_link."?glass=".$item->id;
+        $img = explode(".", $item->Photo)[0]."tz.png";
         echo "<td>".$item->id ."</td>";
         echo "<td>".$item->product_name ."</td>";
+        echo "<td><image src=images/"."$img"."></td>";
+        echo "<td><a href="."$more".">more</a></td>";
     echo "</tr>";
 
     $record_index ++;
@@ -23,6 +31,11 @@ echo "</table>";
 <div> 
     <a href="<?php echo $previous_link;  ?>"> << Prev </a> | <a href="<?php echo $next_link;  ?>">  Next >> </a>
 </div>
+
+<form >
+    <input type="text" placeholder="search by name" name="search">
+    <input type="submit" value="search">
+</form>
 
 
 
